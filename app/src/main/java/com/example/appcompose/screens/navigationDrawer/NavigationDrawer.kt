cec450a5083594e357b38f9624c8bd66dd378a59
Prasoon Agrawal <a.prasoon23@gmail.com>
2023-06-09 13:44:38 +0530

    Bottom Navigation Drawer

--- app/src/main/java/com/example/appcompose/screens/navigationDrawer/NavigationDrawer.kt
+++ app/src/main/java/com/example/appcompose/screens/navigationDrawer/NavigationDrawer.kt
@@ -61,7 +61,7 @@ fun NavigationDrawer(navController: NavHostController){
                 Spacer(modifier = Modifier.height(8.dp))
                 CardItem2(title = "Bottom Drawer",route = Destination.BottomDrawerSample.route,navController)
                 Spacer(modifier = Modifier.height(8.dp))
-                HyperlinkText(fullText = "SourceCode", linkText = listOf("SourceCode") , hyperlinks = listOf("https://www.google.co.in/") )
+                HyperlinkText(fullText = "SourceCode", linkText = listOf("SourceCode") , hyperlinks = listOf("https://github.com/hey-agrawal/BasicCode/blob/main/BottomNavigationDrawer.kt") )
                 Spacer(modifier = Modifier.height(8.dp))
             }
         }
