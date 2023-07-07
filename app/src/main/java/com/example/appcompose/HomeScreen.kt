542acefbf581910751622c9ff5c7f6ed2dd45514
Prasoon Agrawal <a.prasoon23@gmail.com>
2023-07-07 14:56:11 +0530

    Initial commit

---
+++ app/src/main/java/com/example/appcompose/HomeScreen.kt
@@ -0,0 +1,55 @@
+package com.example.appcompose
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.Scaffold
+import androidx.compose.material.Text
+import androidx.compose.material.TopAppBar
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.AccountBox
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import androidx.navigation.NavHostController
+//import androidx.compose.foundation.layout.Column
+//import androidx.compose.foundation.layout.Spacer
+//import androidx.compose.foundation.layout.fillMaxSize
+//import androidx.compose.foundation.layout.height
+//import androidx.compose.foundation.layout.padding
+//import androidx.compose.material.Scaffold
+//import androidx.compose.material.Text
+//import androidx.compose.material.TopAppBar
+//import androidx.compose.material.icons.Icons
+//import androidx.compose.material.icons.filled.AccountBox
+//import androidx.compose.runtime.Composable
+//import androidx.compose.ui.Modifier
+//import androidx.compose.ui.unit.dp
+//import androidx.navigation.NavHostController
+
+@Composable
+fun HomeScreen(navController: NavHostController) {
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                title = { Text(text = "Home") }
+            )
+        },
+        content = {
+            Column(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(16.dp)
+            ) {
+                CardItem("Screen2", Icons.Default.AccountBox, Destination.ScreenSecond.route,navController)
+                Spacer(modifier = Modifier.height(16.dp))
+                CardItem("Screen3", Icons.Default.AccountBox, Destination.ScreenThird.route,navController
+                )
+                Spacer(modifier = Modifier.height(16.dp))
+                CardItem("Screen4", Icons.Default.AccountBox, Destination.ScreenFourth.route,navController)
+            }
+        }
+    )
+}
+
---
+++ app/src/main/java/com/example/appcompose/MainActivity.kt
@@ -0,0 +1,82 @@
+package com.example.appcompose
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material.Card
+import androidx.compose.material.Icon
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.vector.ImageVector
+
+import androidx.compose.ui.unit.dp
+import androidx.navigation.NavController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.example.appcompose.ui.theme.AppComposeTheme
+
+sealed class Destination(val route: String) {
+    object ScreenFirst : Destination("ScreenFirst")
+    object ScreenSecond : Destination("ScreenSecond")
+    object ScreenThird : Destination("ScreenThird")
+    object ScreenFourth : Destination("ScreenFourth")
+    object ScreenFive : Destination("ScreenFive")
+
+
+}
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            AppComposeTheme {
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colors.background
+                ) {
+                    val navController = rememberNavController()
+                    NavHost(navController = navController, startDestination = Destination.ScreenFirst.route) {
+                        composable(Destination.ScreenFirst.route) { HomeScreen(navController) }
+                        composable(Destination.ScreenSecond.route) { Screen2(navController) }
+                        composable(Destination.ScreenThird.route) { Screen3(navController) }
+                        composable(Destination.ScreenFourth.route) { Screen4(navController) }
+                        composable(Destination.ScreenFive.route) { Screen5(navController) }
+                    }
+                }
+        }
+}}}
+    @Composable
+    fun CardItem(title: String, icon: ImageVector, route: String, navController: NavController) {
+        Card(
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(120.dp)
+                .clickable { navController.navigate(route) },
+            elevation = 8.dp
+        ) {
+            Column(
+                modifier = Modifier
+
+                    .padding(16.dp)
+                    .fillMaxSize(),
+                verticalArrangement =  Arrangement.Center,
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                Icon(imageVector = icon, contentDescription = "Icon", modifier = Modifier.size(48.dp))
+                Text(text = title, style = MaterialTheme.typography.h6, modifier = Modifier.padding(top = 8.dp))
+            }
+        }}
+
