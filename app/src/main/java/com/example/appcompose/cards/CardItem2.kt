fbecee73662fff2e3ad8533fc50772dc81cc0193
Prasoon Agrawal <a.prasoon23@gmail.com>
2023-07-07 21:32:30 +0530

    description and icon set

---
+++ app/src/main/java/com/example/appcompose/cards/CardItem2.kt
@@ -0,0 +1,43 @@
+package com.example.appcompose.cards
+
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material.Card
+import androidx.compose.material.Icon
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.unit.dp
+import androidx.navigation.NavController
+
+@Composable
+fun CardItem2(title: String,route: String, navController: NavController) {
+    Card(
+        modifier = Modifier
+            .fillMaxWidth()
+            .height(120.dp)
+            .clickable { navController.navigate(route) },
+        elevation = 8.dp
+    ) {
+        Column(
+            modifier = Modifier
+
+                .padding(16.dp)
+                .fillMaxSize(),
+            verticalArrangement =  Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Text(text = title, style = MaterialTheme.typography.h6, modifier = Modifier.padding(top = 8.dp))
+        }
+    }
+}
+
